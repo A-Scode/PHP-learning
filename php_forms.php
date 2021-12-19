6e7d2837d4ebdd4aea6444b3149e5f9474f1ba52
shouryaraj <shouryarajsinghgoud@gmail.com>
2021-12-19 17:26:06 +0530

    file handling and uploading complete

--- php_forms.php
+++ php_forms.php
@@ -4,30 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta http-equiv ="refresh" content = "3"> -->
     <link rel="stylesheet" type="text/css" href= "css/php_forms.css">
     <title>PHP Forms</title>
 </head>
 <body>
     <h1 align = "center">PHP Forms</h1>
-    <form method = "POST" action = "check_form.php" >
+    <form method = "POST" action = "check_form.php" name = "register" enctype= "multipart/form-data">
+        <label for = "profile_photo" style="display:flex;place-items:center;justify-content:center;" ><img src = "css/unkownUser.svg" id = "photo_label" /></label>
+        <input  type="file" name="photo"  id="profile_photo" accept = ".jpg , .jpeg , .png , .svg" hidden />
         <label for="firstname">FirstName</label>
-        <input id = "firstname" type="text" max=20  />
+        <input name = "firstname" id = "firstname" type="text" max=20  />
         <label for = "lastname" >LastName</label>
-        <input id = "lastname"  type= "text" max=20 />
+        <input name = "lastname" id = "lastname"  type= "text" max=20 />
         <label for="city">Select City</label>
-        <select id = "city">
+<select id = "city">
         <option value ="Indore">Indore</option>
         <option value="Bhopal">Bhopal</option>
         <option value="Jharkhand">Jharkhand</option>
         <option value = "Khandwa">Khandwa</option>
 </select>
+        <label >Gender</label>
+        <div class="gender_radio">
+            <div><input  type="radio" name="gender" id="Male"  value="Male"><label for="Male">Male</label></div>
+            <div><input  type="radio" name="gender" id="Female"  value="Female"><label for="Female">Female</label></div>
+            <div><input  type="radio" name="gender" id="Other"  value="Other"><label for="Other">Other</label></div>
+        </div>
 
-
-        <input type ="submit" />
+        <input  type ="submit" />
     </form>
 
     <?php
     ?>
     
 </body>
+<script src="scripts/php_forms.js"></script>
 </html>
